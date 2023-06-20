7ee96839efb308f58d5d4c927b493e7e0990bb95
Mohd Zakir <35057482+mohdzakir12@users.noreply.github.com>
2023-06-20 21:38:56 +0530

    using data eks cluster to retrive node group name

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "aws_eks_cluster_auth" "clutertoken" {
 
 data "aws_eks_node_group" "ng_info" {
   cluster_name     = data.aws_eks_cluster.example.name
-  node_group_name  = data.aws_eks_cluster_auth.clutertoken.data["aws-auth"].mapRoles[1].groups[0]
+  node_group_name  = data.aws_eks_cluster.example.node_groups[0].name
 }
 
 locals {
